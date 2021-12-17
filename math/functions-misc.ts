da3f781500cca2739415361a947bfd634f617044
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2021-12-17 02:55:24 +0100

    lateCosinus deserve to be saved

---
+++ math/functions-misc.ts
@@ -0,0 +1,3 @@
+
+// https://www.desmos.com/calculator/jrkunm5kdn?lang=fr
+export const lateCosinus = (x: number, t = 0.5) => Math.cos(((1 + t) * x - t) * Math.PI / 2)
--- math/index.ts
+++ math/index.ts
@@ -1,3 +1,4 @@
 export * from './functions'
+export * from './functions-misc'
 export * from './PRNG'
 export * from './bezier'
