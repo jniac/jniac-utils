3935423ede2231c6031c5efe01dc50642650eead
joseph@ovvo-debian <joseph.merdrignac@gmail.com>
2022-03-09 15:55:24 +0000

    string! (stringMax)

---
+++ string/index.ts
@@ -0,0 +1,11 @@
+
+export const stringMax = (str: string, maxLength = 100, {
+  pattern = '...',
+} = {}) => {
+  const { length } = str
+  if (length < maxLength) {
+    return str
+  }
+  const ending = `${pattern} (${length.toString(10)})`
+  return str.slice(0, maxLength - ending.length) + ending
+}
