8abf451a00406aea532b4adb8c35cc91c6c87e84
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2022-03-16 17:37:38 +0100

    isEmail

--- string/index.ts
+++ string/index.ts
@@ -9,3 +9,6 @@ export const stringMax = (str: string, maxLength = 100, {
   const ending = `${pattern} (${length.toString(10)})`
   return str.slice(0, maxLength - ending.length) + ending
 }
+
+const isEmailRe = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+export const isEmail = (str: string) => isEmailRe.test(str)
