602ab3c13b90a9777dd81204158e868d58dac00c
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2022-03-16 17:50:53 +0100

    ok eslint

--- string/index.ts
+++ string/index.ts
@@ -10,5 +10,6 @@ export const stringMax = (str: string, maxLength = 100, {
   return str.slice(0, maxLength - ending.length) + ending
 }
 
+// eslint-disable-next-line no-useless-escape
 const isEmailRe = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
 export const isEmail = (str: string) => isEmailRe.test(str)
