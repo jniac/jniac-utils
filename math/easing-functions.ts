afb4742828f5325188c4ebe3a0bf4ab8dd28a68d
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2022-05-03 17:46:46 +0200

    TimeHandler is cool I suppose

--- math/easing-functions.ts
+++ math/easing-functions.ts
@@ -27,45 +27,35 @@ export const out6 = (x: number) => 1 - (x = 1 - x) * x * x * x * x * x
 
 // https://www.desmos.com/calculator/chosfesws4
 export const inout = (x:number, p: number = 3, i: number = 0.5) => {
-  if (x < 0) return 0
-  if (x > 1) return 1
-  return (x < i
+  return (x < 0 ? 0 : x > 1 ? 1 : x < i
     ? 1 / Math.pow(i, p - 1) * Math.pow(x, p)
     : 1 - 1 / Math.pow(1 - i, p - 1) * Math.pow(1 - x, p)
   )
 }
 
 export const inout2 = (x: number) => {
-  if (x < 0) return 0
-  if (x > 1) return 1
-  return (x < 0.5
+  return (x < 0 ? 0 : x > 1 ? 1 : x < 0.5
     ? 2 * x * x
     : 1 - 2 * (x = 1 - x) * x
   )
 }
 
 export const inout3 = (x: number) => {
-  if (x < 0) return 0
-  if (x > 1) return 1
-  return (x < 0.5
+  return (x < 0 ? 0 : x > 1 ? 1 : x < 0.5
     ? 4 * x * x * x
     : 1 - 4 * (x = 1 - x) * x * x
   )
 }
 
 export const inout4 = (x: number) => {
-  if (x < 0) return 0
-  if (x > 1) return 1
-  return (x < 0.5
+  return (x < 0 ? 0 : x > 1 ? 1 : x < 0.5
     ? 8 * x * x * x * x
     : 1 - 8 * (x = 1 - x) * x * x * x
   )
 }
 
 export const inout5 = (x: number) => {
-  if (x < 0) return 0
-  if (x > 1) return 1
-  return (x < 0.5
+  return (x < 0 ? 0 : x > 1 ? 1 : x < 0.5
     ? 16 * x * x * x * x * x
     : 1 - 16 * (x = 1 - x) * x * x * x * x
   )
