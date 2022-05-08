2baddcc12a125d19daaee920db460e4a931b5a30
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2022-05-08 11:04:54 +0200

    not so cool, ObservableObject is a tricky concept
    "valueOld" is almost useless since it only represents the very last
    previous state. To check prop old value there's no other solution to
    create a scope by prop to watch changes.

--- observables/ObservableObject.ts
+++ observables/ObservableObject.ts
@@ -2,6 +2,28 @@
 import { deepClone, deepPartialEquals, deepPartialCopy, deepGet } from '../object/clone'
 import { Observable, SetValueOptions } from './Observable'
 
+// https://stackoverflow.com/questions/41980195/recursive-partialt-in-typescript
+type RecursivePartial<T> = {
+  [P in keyof T]?:
+    T[P] extends (infer U)[] ? RecursivePartial<U>[] :
+    T[P] extends object ? RecursivePartial<T[P]> :
+    T[P]
+};
+
+/**
+ * Tricky concept.
+ * 
+ * Be aware that valueOld only refers to the previous last change state.
+ * This won't work:
+ * 
+ * ```
+ * const x = new ObservableObject({ name: 'foo', age: 7 })
+ * x.updateValue({ age: 8 })
+ * x.valueOld.age // 7 (ok)
+ * x.updateValue({ name: 'bar })
+ * x.valueOld.age // 8 (ok but expected?)
+ * ``` 
+ */
 export class ObservableObject<T> extends Observable<T> {
 
   constructor(initialValue: T) {
@@ -12,7 +34,7 @@ export class ObservableObject<T> extends Observable<T> {
   /**
    * Same as setValue but WITHOUT changing the inner value reference, but its properties only (deep copy).
    */
-  updateValue(value: Partial<T> | ((v: T) => Partial<T>), { 
+  updateValue(value: RecursivePartial<T> | ((v: T) => RecursivePartial<T>), { 
     ignoreCallbacks = false, 
     owner = null 
   }: SetValueOptions = {}) {
@@ -22,7 +44,7 @@ export class ObservableObject<T> extends Observable<T> {
     }
 
     if (typeof value === 'function') {
-      value = (value as (value: T) => Partial<T>)(this.value)
+      value = (value as (value: T) => RecursivePartial<T>)(this.value)
     }
 
     const hasChanged = deepPartialEquals(value, this.value) === false
@@ -43,17 +65,14 @@ export class ObservableObject<T> extends Observable<T> {
     return deepGet(this.value, path)
   }
 
-  getPropValueOld(path: string | (string | number | symbol)[]) {
-    return deepGet(this.valueOld, path)
-  }
-
   onPropChange<V = any>(path: string | (string | number | symbol)[], callback: (value: V, valueOld: V, target: ObservableObject<T>) => void)
   {
+    let valueOld = this.getPropValue(path)
     return this.onChange(() => {
       const value = this.getPropValue(path)
-      const valueOld = this.getPropValueOld(path)
       if (value !== valueOld) {
         callback(value, valueOld, this)
+        valueOld = value
       }
     })
   }
