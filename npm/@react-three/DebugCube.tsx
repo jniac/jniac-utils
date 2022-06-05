b6296dda38655391791d1096d952ed990a231d9a
jniac <joseph.merdrignac@gmail.com>
2022-06-05 08:21:48 +0200

    shaders (2D sdf from iq), fix DebugCube imports

--- npm/@react-three/DebugCube.tsx
+++ npm/@react-three/DebugCube.tsx
@@ -1,9 +1,10 @@
 // @ts-ignore (ignore none-existing module, of course if module does not exist this file should not be imported)
 import { useState } from 'react'
+// @ts-ignore (ignore none-existing module, of course if module does not exist this file should not be imported)
 import * as THREE from 'three'
-import { useRefComplexEffects } from 'some-utils/react'
-import { time } from 'some-utils/npm/@react-three/TimeHandler'
-import { radian } from 'some-utils/math'
+import { useRefComplexEffects } from '../../react'
+import { radian } from '../../math'
+import { time } from '.'
 
 export const DebugCube = ({ rotate = false }) => {
 
---
+++ shaders/index.ts
@@ -0,0 +1,1 @@
+export * from './sdf-2d'
