8eb1e61f74172099f21c5ab64c0d70c510d1fcf6
jniac <joseph.merdrignac@gmail.com>
2022-06-14 16:47:10 +0200

    clamp01 belongs to "utils"

--- shaders/glsl/utils.ts
+++ shaders/glsl/utils.ts
@@ -1,5 +1,9 @@
 export const glsl_utils = /* glsl */`
 
+  float clamp01(float x) {
+    return x < 0.0 ? 0.0 : x > 1.0 ? 1.0 : x;
+  }
+  
   float inverseLerp(float a, float b, float x) {
     return (x - a) / (b - a);
   }
