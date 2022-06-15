4b0ec9c5b90e0d20f91ace44818a848fcd9d3db0
jniac <joseph.merdrignac@gmail.com>
2022-06-15 11:42:16 +0200

    'reload' options

--- npm/react/router/dom/Link.tsx
+++ npm/react/router/dom/Link.tsx
@@ -4,18 +4,31 @@ import { RouterContext } from '../Router'
 
 export const Link = React.forwardRef<HTMLAnchorElement, {
   to: string
+  reload?: boolean
 } & React.HTMLAttributes<HTMLAnchorElement>>(({
-  to, 
+  to,
+  reload = false,
   children, 
   ...props
 }, ref) => {
   const { baseUrl } = React.useContext(RouterContext)
   const onClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    
     e.preventDefault()
+
     if (baseUrl && to.startsWith('/')) {
       // baseUrl injection
-      setPathname(`/${baseUrl}${to}`)
-    } else {
+      const pathname = `/${baseUrl}${to}`
+      if (reload) {
+        const url = (window.location.origin + pathname.substring(1))
+        window.open(url, '_self')
+      }
+      else {
+        setPathname(pathname)
+      }
+    } 
+    
+    else {
       setPathname(to)
     }
   }
