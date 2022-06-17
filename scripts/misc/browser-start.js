f5d3204b565c0a83746aa12b1446de5234bff9b6
jniac <joseph.merdrignac@gmail.com>
2022-06-17 10:52:51 +0200

    better start (chrome yahaaa! + shebang & chmod)

--- scripts/misc/browser-start.js
+++ scripts/misc/browser-start.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 // Because BROWSER="Google Chrome Dev" (or any other syntax) doesn't work.
 // usage (.env):
 // BROWSER=src/some-utils/scripts/misc/browser-start.js
@@ -7,7 +8,7 @@ const { spawn } = require('child_process')
 // @ts-ignore
 const os = require('os')
 
-const [,,url] = process.argv
+const [,,url = ''] = process.argv
 
 /**
  * 
@@ -20,8 +21,25 @@ const trySpawn = (...spawnArgs) => new Promise(resolve => {
   child.on('error', () => resolve({ ok: false, child }))
 })
 
+const echoSpawn = (...spawnArgs) => new Promise((resolve, reject) => {
+  const child = spawn(...spawnArgs)
+  const result = []
+  child.stdout.on('data', data => {
+    result.push(data)
+  })
+  child.on('close', () => {
+      resolve(result.join(''))
+  })
+  child.on('error', error => reject(error))
+})
+
 const main = async () => {
   if (os.type() === 'Darwin') {
+    // Ok you're right, it's a weird option, it allows me to run a Google Chrome instance dedicated to development.
+    const userDir = await echoSpawn('echo', [`~`])
+    if ((await trySpawn('/Applications/Google Chrome Yahaaa!.app/Contents/MacOS/Google Chrome', [`--user-data-dir=${userDir}/chrome-yahaa`, url])).ok) {
+      return
+    }
     if ((await trySpawn('/Applications/Google Chrome Dev.app/Contents/MacOS/Google Chrome Dev', [url])).ok) {
       return
     }
