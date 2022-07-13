3401661b2d527cec590aabe1a979f5184c180cf3
jniac <joseph.merdrignac@gmail.com>
2022-07-13 17:40:13 +0200

    useSearchState is a good idea but lacks some features right now (auto refresh when history pop state)

--- npm/react/hooks/state.ts
+++ npm/react/hooks/state.ts
@@ -27,3 +27,43 @@ export function useLocalStorageState<T extends object>(identifier: string, getIn
 
   return [state, setLocalStorageState]
 }
+
+export function useSearchState<T extends object>(getIntialState: () => T): [T, (value: T) => void]
+export function useSearchState<T extends object>(initialState: T): [T, (value: T) => void]
+export function useSearchState<T extends object>(getIntialState: () => T) {
+
+  if (typeof getIntialState === 'object') {
+    const initialState = getIntialState
+    getIntialState = () => initialState
+  }
+
+  const [state, setState] = useState(() => {
+    const state = getIntialState() as any
+    const url = new window.URL(document.location.href)
+    for (const [key, value] of url.searchParams.entries()) {
+      switch (typeof state[key]) {
+        case 'boolean':
+          state[key] = /true/i.test(value)
+          break
+        case 'number':
+          state[key] = parseFloat(value)
+          break
+        default:
+          state[key] = value
+          break
+      }
+    }
+    return state as T
+  })
+
+  const setSearchState = useCallback((state: T) => {
+    const url = new window.URL(document.location.href)
+    for (const key in state) {
+      url.searchParams.set(key, String(state[key]))
+    }
+    window.history.pushState(null, '', url.href)
+    setState(state)
+  }, [])
+
+  return [state, setSearchState]
+}
