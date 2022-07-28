8e758ddd5f43f4d58c4003ec94ad2e5d325ac876
jniac <joseph.merdrignac@gmail.com>
2022-07-28 11:21:16 +0200

    range is useful from times to times

---
+++ iterators/range.ts
@@ -0,0 +1,30 @@
+
+export function range(max: number, options?: { reverse?: boolean, step?: number }): Generator<number>
+export function range(min: number, max: number, options?: { reverse?: boolean, step?: number }): Generator<number>
+export function* range(...args: any[]) {
+  const optionsIndex = args.findIndex(v => typeof v === 'object')
+  const { 
+    reverse = false, 
+    step = 1,
+  } = optionsIndex !== -1 ? args.splice(optionsIndex, 1)[0] : {}
+  let min = 0, max = 0
+  if (args.length === 1) {
+    max = args[0]
+  } 
+  else if (args.length === 2) {
+    min = args[0]
+    max = args[1]
+  } 
+  else {
+    throw new Error(`Invalid usage (Typescript is missing).`)
+  }
+  if (reverse === false) {
+    for (let i = min; i < max; i += step) {
+      yield i
+    }
+  } else {
+    for (let i = max - ((max - min) % step); i >= min; i -= step) {
+      yield i
+    }
+  }
+}
