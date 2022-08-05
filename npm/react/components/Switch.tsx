84e225392686824784d7527221436cd3ac6660ba
jniac <joseph.merdrignac@gmail.com>
2022-08-05 10:01:37 +0200

    content can have props

--- npm/react/components/Switch.tsx
+++ npm/react/components/Switch.tsx
@@ -3,14 +3,19 @@ import * as Animation from '../../../Animation'
 import { Observable } from '../../../observables'
 import { useComplexEffects } from '../hooks'
 
+type Item = React.ElementType | [React.ElementType, Record<string, any>]
+const solveItem = (item: Item): [React.ElementType, Record<string, any>] => {
+  return Array.isArray(item) ? item : [item, {}]
+}
+
 export interface SwitchChildProps { 
   entering?: boolean
   leaving?: boolean
 }
 
-export interface SwitchProps<T> {
+export interface Props<T> {
   index?: number
-  items?: React.ElementType[]
+  items?: Item[]
   transitionDuration?: number
   onTransition?: (entering: T | null, leaving: T | null, progress: number) => void
   debugDisplayAll?: boolean
@@ -22,7 +27,7 @@ export const Switch = <T extends unknown>({
   transitionDuration = 0.8,
   debugDisplayAll = false,
   onTransition,
-}: SwitchProps<T>) => {
+}: Props<T>) => {
 
   const ref1 = React.useRef<T>(null)
   const ref2 = React.useRef<T>(null)
@@ -58,12 +63,17 @@ export const Switch = <T extends unknown>({
   const render2 = inverse || transition
   const index1 = !inverse ? indexObs.value : indexObs.valueOld
   const index2 = inverse ? indexObs.value : indexObs.valueOld
-  const Content1 = items[index1]
-  const Content2 = items[index2]
+  const [Content1, props1] = solveItem(items[index1])
+  const [Content2, props2] = solveItem(items[index2])
 
   if (debugDisplayAll) {
     return (
-      <>{items.map((Item, index) => <Item key={index} />)}</>
+      <>{items.map((item, index) => {
+        const [Item, props] = solveItem(item)
+        return (
+          <Item key={index} {...props} />
+        )
+      })}</>
     )
   }
 
@@ -72,6 +82,7 @@ export const Switch = <T extends unknown>({
       {(render1 && Content1) && (
         <Content1 
           ref={ref1}
+          {...props1}
           entering={transition && !inverse}
           leaving={transition && inverse}
         />
@@ -79,6 +90,7 @@ export const Switch = <T extends unknown>({
       {(render2 && Content2) && (
         <Content2
           ref={ref2}
+          {...props2}
           entering={transition && inverse}
           leaving={transition && !inverse}
         />
