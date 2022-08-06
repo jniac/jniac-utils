a955526ad8a4c476e21cc02eadc1f2f71d3266e3
jniac <joseph.merdrignac@gmail.com>
2022-08-06 09:34:00 +0200

    baseUrl as RegExp too & pathnameTransform

--- npm/react/router/Router.tsx
+++ npm/react/router/Router.tsx
@@ -2,22 +2,31 @@ import React from 'react'
 import { getPathname } from '../../../router'
 
 export const RouterContext = React.createContext({
-  baseUrl: '',
+  baseUrl: '' as string | RegExp,
+  pathnameTransform: null as null | ((pathname: string) => string),
   getPathname: () => '' as string,
 })
 
+const cleanPathname = (value: string) => {
+  return value.replace(/\/{2,}/g, '/').replace(/\/$/, '') || '/'
+}
+
 export const Router: React.FC<{
-  baseUrl?: string
+  baseUrl?: string | RegExp
+  pathnameTransform?: null | ((pathname: string) => string),
   children?: React.ReactNode
 }> = ({
   baseUrl = '',
+  pathnameTransform = null,
   children,
 }) => {
 
   const context = {
     baseUrl,
+    pathnameTransform,
     getPathname: () => {
-      return getPathname().replace(baseUrl, '') || '/'
+      const pathname = cleanPathname(getPathname().replace(baseUrl, ''))
+      return pathnameTransform ? cleanPathname(pathnameTransform(pathname)) || '/' : pathname
     },
   }
 
