e92d89d5d03f83dca9a7e6af5c32c2486336cda2
jniac <joseph.merdrignac@gmail.com>
2022-10-06 10:03:51 +0200

    link accepts "onClick" + check for "isClick()"

--- npm/react/router/dom/Link.tsx
+++ npm/react/router/dom/Link.tsx
@@ -1,22 +1,28 @@
 import React from 'react'
+import { isClick } from '../../../../dom'
 import { RouterContext } from '../Router'
 
 export const Link = React.forwardRef<HTMLAnchorElement, {
   to: string
   reload?: boolean
+  onClick?: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void
 } & React.HTMLAttributes<HTMLAnchorElement>>(({
   to,
   reload = false,
+  onClick,
   children,
   ...props
 }, ref) => {
   const { go } = React.useContext(RouterContext)
-  const onClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-    e.preventDefault()
-    go(to)
+  const _onClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    if (isClick()) {
+      event.preventDefault()
+      go(to)
+      onClick?.(event)
+    }
   }
   return (
-    <a {...props} ref={ref} href={to} onClick={onClick}>
+    <a {...props} ref={ref} href={to} onClick={_onClick}>
       {children}
     </a>
   )
