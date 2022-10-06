451e99dcb3000aa22c14382bb3e889236ea18b4d
jniac <joseph.merdrignac@gmail.com>
2022-10-06 07:38:12 +0200

    pinch: split debug from core script

--- dom/handle-events/handlePointer/pinch.ts
+++ dom/handle-events/handlePointer/pinch.ts
@@ -1,4 +1,5 @@
-import { Point } from 'some-utils/geom'
+import { Point } from '../../../geom'
+import { destroyDebugDisplay, updateDebugDisplay } from './pinch-debug'
 
 type PinchState = {
   center: Point
@@ -45,59 +46,6 @@ export const isPinchListening = (options: PinchOptions) => {
   )
 }
 
-const updateDebugDisplay = (point0: Point, point1: Point) => {
-  const createPin = (id: string, html: string) => {
-    const div = document.createElement('div')
-    div.id = id
-    document.body.append(div)
-    div.style.position = 'fixed'
-    div.style.width = '32px'
-    div.style.height = '32px'
-    div.style.border = 'solid 2px #0006'
-    div.style.borderRadius = '50%'
-    div.style.transform = 'translate(-50%, -50%)'
-    div.style.backgroundColor = '#fff6'
-    div.style.display = 'flex'
-    div.style.justifyContent = 'center'
-    div.style.alignItems = 'center'
-    div.innerHTML = html
-    return div
-  }
-  const createCenter = (id: string) => {
-    const div = document.createElement('div')
-    div.id = id
-    document.body.append(div)
-    div.style.position = 'fixed'
-    div.style.width = '16px'
-    div.style.height = '16px'
-    div.style.borderRadius = '50%'
-    div.style.transform = 'translate(-50%, -50%)'
-    div.style.backgroundColor = '#0006'
-    return div
-  }
-  const id0 = 'handlePinch-debug-pin-0'
-  const id1 = 'handlePinch-debug-pin-1'
-  const id2 = 'handlePinch-debug-center'
-  const pin0 = document.querySelector(`#${id0}`) as HTMLDivElement ?? createPin(id0, '0')
-  const pin1 = document.querySelector(`#${id1}`) as HTMLDivElement ?? createPin(id1, '1')
-  const center = document.querySelector(`#${id2}`) as HTMLDivElement ?? createCenter(id2)
-  pin0.style.left = `${point0.x}px`
-  pin0.style.top = `${point0.y}px`
-  pin1.style.left = `${point1.x}px`
-  pin1.style.top = `${point1.y}px`
-  center.style.left = `${(point0.x + point1.x) / 2}px`
-  center.style.top = `${(point0.y + point1.y) / 2}px`
-}
-
-const destroyDebugDisplay = () => {
-  const id0 = 'handlePinch-debug-pin-0'
-  const id1 = 'handlePinch-debug-pin-1'
-  const id2 = 'handlePinch-debug-center'
-  document.querySelector(`#${id0}`)?.remove()
-  document.querySelector(`#${id1}`)?.remove()
-  document.querySelector(`#${id2}`)?.remove()
-}
-
 export const handlePinch = (element: HTMLElement | Window, options: PinchOptions) => {
   const {
     capture = false,
@@ -105,11 +53,13 @@ export const handlePinch = (element: HTMLElement | Window, options: PinchOptions
     fakePinchWithShiftKey = true,
     debugPinch = false,
   } = options
+
   const info: InternalPinchInfo = { frame: 0 }
   let isPinch = false
   let isFakePinch = false
   let fakePinchStartPoint: Point | null = null
   let points: Point[] = []
+
   const update = (point0: Point, point1: Point, isFakePinch: boolean) => {
     info.old = info.current
     const center = Point.add(point0, point1).multiplyScalar(.5)
@@ -136,6 +86,7 @@ export const handlePinch = (element: HTMLElement | Window, options: PinchOptions
     options.onPinch?.(info as PinchInfo)
     info.frame++
   }
+
   const stop = () => {
     options.onPinchStop?.(info as PinchInfo)
     info.frame = 0
@@ -145,6 +96,7 @@ export const handlePinch = (element: HTMLElement | Window, options: PinchOptions
     fakePinchStartPoint = null
     destroyDebugDisplay()
   }
+
   const onTouch = (event: TouchEvent) => {
     points = [...event.touches].map(touch => new Point(touch.clientX, touch.clientY))
     isPinch = points.length === 2
@@ -156,6 +108,7 @@ export const handlePinch = (element: HTMLElement | Window, options: PinchOptions
       stop()
     }
   }
+
   let frameId = -1
   const frameUpdate = () => {
     frameId = window.requestAnimationFrame(frameUpdate)
@@ -187,14 +140,17 @@ export const handlePinch = (element: HTMLElement | Window, options: PinchOptions
       }
     }
   }
+
   const target = element as HTMLElement // Fooling typescript.
   target.addEventListener('touchmove', onTouch, { capture, passive })
   target.addEventListener('touchend', onTouch, { capture, passive })
   frameId = window.requestAnimationFrame(frameUpdate)
+
   const destroy = () => {
     target.removeEventListener('touchmove', onTouch, { capture })
     target.removeEventListener('touchend', onTouch, { capture })
     window.cancelAnimationFrame(frameId)
   }
+  
   return { destroy }
 }
