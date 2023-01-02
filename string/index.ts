4b9482fb2e4023bba8b9629934e5aa371e5d3850
jniac <joseph.merdrignac@gmail.com>
2023-01-03 00:48:49 +0100

    replaceLineLeadingSpaces is useful

--- string/index.ts
+++ string/index.ts
@@ -13,3 +13,26 @@ export const stringMax = (str: string, maxLength = 100, {
 // eslint-disable-next-line no-useless-escape
 const isEmailRe = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
 export const isEmail = (str: string) => isEmailRe.test(str)
+
+export const replaceLineLeadingSpaces = (str: string, pattern: string, { failSilently = false } = {}) => {
+  const lines = str.split('\n').filter(line => /\S/.test(line))
+  if (lines.length === 0) {
+    return ''
+  }
+  const [leadingSpaces] = lines[0].match(/^\s+/)!
+  for (let index = 0, max = lines.length; index < max; index++) {
+    const line = lines[index]
+    if (line.startsWith(leadingSpaces)) {
+      lines[index] = line.substring(leadingSpaces.length)
+    } else {
+      if (failSilently === false) {
+        throw new Error(`Invalid string! Line #${index} does not start with "${leadingSpaces}" (leading spaces).`)
+      }
+    }
+  }
+  return lines.join('\n')
+}
+
+export const removeLineLeadingSpaces = (str: string, { failSilently = false } = {}) => {
+  return replaceLineLeadingSpaces(str, '', { failSilently })
+}
