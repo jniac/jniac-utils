ac772a66a1a07e9cb926edcc263e01d301815578
jniac <joseph.merdrignac@gmail.com>
2023-01-13 18:28:32 +0100

    deepLerpUnclamped + deepLerp

---
+++ math/object.ts
@@ -0,0 +1,23 @@
+
+export const deepLerpUnclamped = (from: any, to: any, out: any, alpha: number) => {
+  for (const key in from) {
+    const fromValue = from[key]
+    switch (typeof fromValue) {
+      case 'number': {
+        out[key] = fromValue + (to[key] - fromValue) * alpha
+        break
+      }
+      case 'object': {
+        if (fromValue !== null) {
+          deepLerpUnclamped(fromValue, to[key], out[key], alpha)
+        }
+        break
+      }
+    }
+  }
+}
+
+export const deepLerp = (from: any, to: any, out: any, alpha: number) => {
+  alpha = alpha < 0 ? 0 : alpha > 1 ? 1 : alpha
+  deepLerpUnclamped(from, to, out, alpha)
+}
