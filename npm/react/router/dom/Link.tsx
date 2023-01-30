dcfb005409c2f925d5a7837360da05b31fd1ab91
jniac <joseph.merdrignac@gmail.com>
2023-01-30 10:51:29 +0100

    react/router: Link has more option, fix tap

--- npm/react/router/dom/Link.tsx
+++ npm/react/router/dom/Link.tsx
@@ -5,11 +5,16 @@ import { RouterContext } from '../Router'
 export const Link = React.forwardRef<HTMLAnchorElement, {
   to: string
   reload?: boolean
+  /** Should we prevent the navigation and let the callbacks do the job? Default is false. */
+  preventNavigation?: boolean
   onClick?: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void
+  onLink?: () => void
 } & React.HTMLAttributes<HTMLAnchorElement>>(({
   to,
   reload = false,
+  preventNavigation = false,
   onClick,
+  onLink,
   children,
   ...props
 }, ref) => {
@@ -19,10 +24,11 @@ export const Link = React.forwardRef<HTMLAnchorElement, {
       event.preventDefault()
       go(to)
       onClick?.(event)
+      onLink?.()
     }
   }
   return (
-    <a {...props} ref={ref} href={`${baseUrl}${to}`} onClick={_onClick}>
+    <a {...props} ref={ref} href={`${baseUrl}${to}`} onPointerUp={_onClick}>
       {children}
     </a>
   )
