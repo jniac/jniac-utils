2b6a09b575b409b854c981b8e9f014fbc71fadba
jniac <joseph.merdrignac@gmail.com>
2023-02-09 19:46:39 +0100

    cross3 is threejs redundant, but ... threejs is not always here

--- math/geom-functions.ts
+++ math/geom-functions.ts
@@ -1,6 +1,13 @@
 
 export const dotProduct = (ux: number, uy: number, vx: number, vy: number) => ux * vx + uy * vy
 
+export const cross3 = <T extends { x: number, y: number, z: number }>(ux: number, uy: number, uz: number, vx: number, vy: number, vz: number, out: T): T => {
+  out.x = uy * vz - uz * vy
+  out.y = uz * vx - ux * vz
+  out.z = ux * vy - uy * vx
+  return out
+}
+
 export const euclideanDistance2D = (x: number, y: number) => Math.sqrt(x * x + y * y)
 
 export const euclideanDistance3D = (x: number, y: number, z: number) => Math.sqrt(x * x + y * y + z * z)
