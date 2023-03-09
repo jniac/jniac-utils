179931913c48c1d405bb1cdb8cfd5ff954cb34e2
jniac <joseph.merdrignac@gmail.com>
2023-03-09 15:26:43 +0100

    iterators/grid: gridArray

--- iterators/grid.ts
+++ iterators/grid.ts
@@ -3,40 +3,70 @@ export type GridIteration = Readonly<{
   x: number
   y: number
   i: number
+  count: number
   centerX: number
   centerY: number
 }>
-export function grid(column: number, row: number): Generator<GridIteration>
+
+function resolveColRow(...args:any[]): readonly [number, number] {
+  let col = 4
+  let row = 4
+
+  if (args.length === 1 && Array.isArray(args[0])) {
+    [args] = args
+  }
+
+  if (args.length > 1) {
+    [col, row] = args
+  } else {
+    ({ x: col, y: row } = args[0])
+  }
+
+  return [col, row]
+}
+
+export function grid(col: number, row: number): Generator<GridIteration>
 export function grid(size: { x: number, y: number }): Generator<GridIteration>
 export function* grid(...args: any[]) {
-  let row = 4, column = 4
-  if (args.length > 1) {
-    [row, column] = args
-  } else {
-    ({ x: row, y: column } = args[0])
-  }
-  let x = 0, y = 0, i = 0
-  const iteration = {
+  const [col, row] = resolveColRow(args)
+  const count = col * row
+  let x = 0
+  let y = 0
+  let i = 0
+
+  const iteration: GridIteration = {
     get x() { return x },
     get y() { return y },
     get i() { return i },
-    get centerX() { return x - (row - 1) / 2 },
-    get centerY() { return y - (column - 1) / 2 },
+    get count() { return count },
+    get centerX() { return x - (col - 1) / 2 },
+    get centerY() { return y - (row - 1) / 2 },
   }
-  for (y = 0; y < row; y++) {
-    for (x = 0; x < column; x++) {
-      yield iteration
+  
+  for (i = 0; i < count; i++) {
+    x = i % col
+    y = Math.floor(i / col)
+    yield iteration
+  }
+}
+
+export function* centerGrid(colHalf = 4, rowHalf = 4) {
+  let i = 0
+  for (let y = -rowHalf; y < rowHalf + 1; y++) {
+    for (let x = -colHalf; x < colHalf + 1; x++) {
+      yield { x, y, i }
       i++
     }
   }
 }
 
-export function* centerGrid(columnHalf = 4, rowHalf = 4) {
-  let i = 0
-  for (let y = -rowHalf; y < rowHalf + 1; y++) {
-    for (let x = -columnHalf; x < columnHalf + 1; x++) {
-      yield { x, y, i }
-      i++
-    }
+export function gridArray(col: number, row: number): GridIteration[]
+export function gridArray(size: { x: number, y: number }): GridIteration[]
+export function gridArray(...args: any[]): GridIteration[] {
+  const [col, row] = resolveColRow(args)
+  const array: GridIteration[] = new Array(col * row)
+  for (const iteration of grid(col, row)) {
+    array[iteration.i] = { ...iteration }
   }
+  return array
 }
