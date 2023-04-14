0a5b758bb36d71dc4a296055dc469d7fa0db4402
jniac <joseph.merdrignac@gmail.com>
2023-04-14 06:20:52 +0200

    rebase ScoreBasedIndexPool on HighScoreMap

--- misc/ScoreBasedIndexPool/ScoreBasedIndexPool.ts
+++ misc/ScoreBasedIndexPool/ScoreBasedIndexPool.ts
@@ -1,23 +1,4 @@
-class Item {
-  index: number = -1
-  score: number = -1
-  isFree(): boolean {
-    return this.index === -1
-  }
-  set(index: number, score: number): this {
-    this.index = index
-    this.score = score
-    return this
-  }
-  free(): this {
-    return this.set(-1, -1)
-  }
-}
-
-/**
- * Used to sort an array of IndexPoolItem the ascending way.
- */
-const itemComparer = (a: Item, b: Item): number => a.score - b.score
+import { HighScoreMap } from 'some-utils/collections'
 
 class ScoreBasedIndexPool {
   #props: {
@@ -26,8 +7,9 @@ class ScoreBasedIndexPool {
     computeScore: (index: number) => number
     onActivate?: (index: number) => void
     onDeactivate?: (index: number) => void
-    map: Map<number, Item>
-    sortedItems: Item[]
+    useIndexes1: boolean
+    indexes1: HighScoreMap<number>
+    indexes2: HighScoreMap<number>
   }
 
   get poolSize() {
@@ -35,40 +17,11 @@ class ScoreBasedIndexPool {
   }
 
   set poolSize(value: number) {
-    // NOTE: changing the pool size is not trivial, since we have potentially to
-    // invoke callbacks.
-    const {
-      poolSize: currentPoolSize,
-      sortedItems,
-      map,
-      onDeactivate,
-    } = this.#props
-    this.#props.poolSize = value
-    // Add new "free" items:
-    if (value > currentPoolSize) {
-      while (sortedItems.length < value) {
-        sortedItems.push(new Item())
-      }
-    }
-    // Removes exceeding items, and call "onDeactivate" on valid indexes:
-    else if (value < currentPoolSize) {
-      sortedItems.sort(itemComparer)
-      const delta = currentPoolSize - value
-      const deactivateIndexes = new Set<number>()
-      for (let i = 0; i < delta; i++) {
-        const item = sortedItems.shift()!
-        const { index } = item
-        if (index !== -1) {
-          map.delete(index)
-          deactivateIndexes.add(index)
-        }
-      }
-      if (onDeactivate) {
-        for (const index of deactivateIndexes) {
-          onDeactivate(index)
-        }
-      }
-    }
+    this.resizePool(value)
+  }
+
+  get watchSize() {
+    return this.#props.watchSize
   }
 
   constructor(
@@ -80,81 +33,71 @@ class ScoreBasedIndexPool {
       onDeactivate?: (index: number) => void
     },
   ) {
-    const sortedItems = new Array(poolSize).fill(null).map(() => new Item())
     this.#props = {
       poolSize,
       watchSize,
-      sortedItems,
-      map: new Map<number, Item>(),
+      useIndexes1: true,
+      indexes1: new HighScoreMap(poolSize),
+      indexes2: new HighScoreMap(poolSize),
       ...props,
     }
   }
 
+  #getBothIndexes(): [indexes: HighScoreMap<number>, oldIndexes: HighScoreMap<number>] {
+    const {
+      useIndexes1,
+      indexes1,
+      indexes2,
+    } = this.#props
+    return useIndexes1 ? [indexes1, indexes2] : [indexes2, indexes1]
+  }
+  
+  #flipIndexes() {
+    this.#props.useIndexes1 = !this.#props.useIndexes1
+  }
+
+  resizePool(newSize: number) {
+    const [indexes] = this.#getBothIndexes()
+    indexes.resize(newSize)
+  }
+
   update() {
     const {
-      poolSize,
       watchSize,
       computeScore,
       onActivate,
       onDeactivate,
-      sortedItems,
-      map,
     } = this.#props
 
-    const activateIndexes = new Set<number>()
-    const deactivateIndexes = new Set<number>()
+    const [indexes, oldIndexes] = this.#getBothIndexes()
+    this.#flipIndexes()
 
+    indexes.clear()
     for (let index = 0; index < watchSize; index++) {
       const score = computeScore(index)
-      const item = map.get(index)
       if (score > 0) {
-        if (item) {
-          // The index is already activated, update the score (if necessary):
-          if (item.score !== score) {
-            item.score = score
-          }
-        } else {
-          if (map.size < poolSize) {
-            // There is room for new items
-            const item = sortedItems.find(item => item.isFree())!
-            item.set(index, score)
-            map.set(index, item)
-            activateIndexes.add(index)
-          } else {
-            // The pool is full:
-            // - Sort the array to find the lowest score item.
-            // - Compare scores, if the current score is bigger do the update.
-            sortedItems.sort(itemComparer)
-            const item = sortedItems[0]
-            if (score > item.score) {
-              activateIndexes.delete(item.index) // Make sure to remove from here too! (since item may have been activated previously during the same "update" call!)
-              deactivateIndexes.add(item.index)
-              map.delete(item.index)
-              item.set(index, score)
-              map.set(index, item)
-              activateIndexes.add(index)
-            }
-          }
-        }
+        indexes.put(score, index)
       }
-      // If the score is zero, or negative:
-      else {
-        if (item) {
-          deactivateIndexes.add(item.index)
-          map.delete(item.index)
-          item.free()
-        }
+    }
+
+    const deactivate = new Set(oldIndexes.values())
+    const activate = new Set(indexes.values())
+
+    for (const index of deactivate) {
+      if (activate.has(index)) {
+        deactivate.delete(index)
+        activate.delete(index)
       }
     }
 
     // Callbacks:
     if (onDeactivate) {
-      for (const index of deactivateIndexes) {
+      for (const index of deactivate) {
         onDeactivate(index)
       }
     }
     if (onActivate) {
-      for (const index of activateIndexes) {
+      for (const index of activate) {
         onActivate(index)
       }
     }
--- misc/ScoreBasedIndexPool/test.ts
+++ misc/ScoreBasedIndexPool/test.ts
@@ -1,4 +1,11 @@
 import { ScoreBasedIndexPool } from './ScoreBasedIndexPool'
+import { createTest, areEquivalent } from '../../test'
+
+const {
+  test,
+  getCurrentTestName,
+  testDone,
+} = createTest('ScoreBasedIndexPool')
 
 export const testScoreBasedIndexPool = () => {
   // Test state:
@@ -11,46 +18,14 @@ export const testScoreBasedIndexPool = () => {
     head: 0,
     distanceMax: 3,
   }
-  const activatedPosition = new Set<{ value: number }>()
-  const getActivatedPositionString = () => [...activatedPosition].map(i => i.value).join(', ')
-
-  // Test object:
-  const test = {
-    state: {
-      debug: false,
-      currentTestName: '',
-    },
-    debug: (value: boolean = true) => {
-      test.state.debug = value
-      return test
-    },
-    name: (name: string) => {
-      test.state.currentTestName = name
-      return test
-    },
-    compare: (array: number[]) => {
-      const set = new Set(array)
-      for (const position of activatedPosition) {
-        if (set.has(position.value) === false) {
-          throw new Error(
-            `Oops.`
-            + `\n> "${test.state.currentTestName}"`
-            + `\n> (${getActivatedPositionString()})`
-            + `\nThe given array (${array.join(', ')}) does not contains ${position.value}.`)
-        }
-        set.delete(position.value)
-      }
-      if (set.size > 0) {
-        throw new Error(
-          `Oops.`
-          + `\n> "${test.state.currentTestName}"`
-          + `\n> (${getActivatedPositionString()})`
-          + `\nThe given array contains extra entries (${[...set].join(', ')})`)
-      }
-      test.state.currentTestName += ` DONE`
-      return test
-    },
+  const activatedPositions = new Set<{ value: number }>()
+  
+  const activatedPositionsAreEquivalentTo = (positions: number[]) => {
+    return areEquivalent(
+      [...activatedPositions].map(v => v.value).sort(),
+      positions)
   }
+  const activatedPositionToString = () => [...activatedPositions].map(i => i.value).join(', ')
 
   const pool = new ScoreBasedIndexPool(5, positions.length, {
     computeScore: index => {
@@ -60,135 +35,143 @@ export const testScoreBasedIndexPool = () => {
       if (distance <= state.distanceMax) {
         score = 1 / distance
       }
-      if (test.state.debug) {
-        console.log(`#${index}(${position.value}) distance: ${distance} score: ${score.toFixed(3)}`)
-      }
       return score
     },
     onActivate: index => {
       const position = positions[index]
-      activatedPosition.add(position)
+      if (activatedPositions.has(position)) {
+        throw new Error(
+          `onActivate: Oh no!`
+          + `\n> "${getCurrentTestName()}"`
+          + `\n> position "${position.value}" is already in (${activatedPositionToString()})`)
+      }
+      activatedPositions.add(position)
     },
     onDeactivate: index => {
       const position = positions[index]
-      if (test.state.debug) {
-        debugger
+      if (activatedPositions.has(position) === false) {
+        throw new Error(
+          `onDeactivate: Oh no!`
+          + `\n> "${getCurrentTestName()}"`
+          + `\n> position "${position.value}" is not in (${activatedPositionToString()})`)
       }
-      activatedPosition.delete(position)
+      activatedPositions.delete(position)
     },
   })
 
-  try {
-    pool.update()
-    test
-      .name('#0 Empty pool.')
-      .compare([])
+  const info = () => `(${state.head}:${state.distanceMax}:${pool.poolSize})`
 
-    // #1. Test activation / deactivation
+  pool.update()
 
-    state.head = 1000
-    pool.update()
-    test
-      .name('#1 activation')
-      .compare([
-        1000, 1001, 1002,
-      ])
+  test(`${info()} Empty pool.`,
+    activatedPositionsAreEquivalentTo([]))
 
-    state.head = 2000
-    pool.update()
-    test
-      .name('#2 deactivation + activation (the pool watch size is not reached).')
-      .compare([
-        2000, 2001, 2002,
-      ])
+  state.head = 1000
+  pool.update()
 
-    state.head = 3000
-    pool.update()
-    test
-      .name('#3 deactivation + activation, more entries (4)')
-      .compare([
-        3000, 3001, 3002, 3003,
-      ])
+  test(`${info()} Activation!`,
+    activatedPositionsAreEquivalentTo([
+      1000, 1001, 1002,
+    ]))
 
-    state.head = 3002
-    pool.update()
-    test
-      .name('#4: clamp to "watchSize" (5)')
-      .compare([
-        3000, 3001, 3002, 3003, 3004,
-      ])
+  state.head = 2000
+  pool.update()
 
-    state.head = 3006
-    pool.update()
-    test
-      .name('#5: activation + deactivation + clamp (make sure the range is centered around 3006)')
-      .compare([
-        3004, 3005, 3006, 3007, 3008,
-      ])
+  test(`${info()} Deactivation + activation (the pool watch size is not reached).`,
+    activatedPositionsAreEquivalentTo([
+      2000, 2001, 2002,
+    ]))
 
-    state.head = 4000
-    pool.update()
-    test
-      .name('#6: everything can be clean in one move')
-      .compare([
-      ])
+  state.head = 3000
+  pool.update()
 
-    // #3. Test with position intermediate update:
+  test(`${info()} Deactivation + activation, more entries (4)`,
+    activatedPositionsAreEquivalentTo([
+      3000, 3001, 3002, 3003,
+    ]))
 
-    positions[0].value = 4000
-    pool.update()
-    test
-      .name('#7: if the original state change, the "index" set follows')
-      .compare([
-        4000,
-      ])
+  state.head = 3002
+  pool.update()
 
-    positions[1].value = 4001
-    pool.update()
-    test
-      .name('#8: follows again')
-      .compare([
-        4000, 4001,
-      ])
+  test(`${info()} Clamp to "watchSize" (5)`,
+    activatedPositionsAreEquivalentTo([
+      3000, 3001, 3002, 3003, 3004,
+    ]))
 
-    positions[0].value = 0
-    positions[2].value = 4002
-    pool.update()
-    test
-      .name('#9: ...and again')
-      .compare([
-        4001, 4002,
-      ])
+  state.head = 3006
+  pool.update()
 
-    state.head = 3006
-    state.distanceMax = 20
-    pool.update()
-    test
-      .name('#10: rollback to 3006 to prepare #11')
-      .compare([
-        3004, 3005, 3006, 3007, 3008,
-      ])
+  test(`${info()} Activation + deactivation + clamp (make sure the range is centered around 3006)`,
+    activatedPositionsAreEquivalentTo([
+      3004, 3005, 3006, 3007, 3008,
+    ]))
 
-    pool.poolSize = 20
-    pool.update()
-    test
-      .name('#11: poolSize has changed (increase)!')
-      .compare([
-        3000, 3001, 3002, 3003, 3004, 3005, 3006, 3007, 3008,
-      ])
+  state.head = 4000
+  pool.update()
 
-    pool.poolSize = 1
-    pool.update()
-    test
-      .name('#11: poolSize has changed (decrease)!')
-      .compare([
-        3006,
-      ])
+  test(`${info()}: Everything can be clean in one move`,
+    activatedPositionsAreEquivalentTo([
+    ]))
 
-    console.log(`Test "testScoreBasedIndexPool" passed.`)
-  } catch (error) {
-    console.log(activatedPosition)
-    console.error(`Test "testScoreBasedIndexPool" failed.`)
-    console.error(error)
-  }
+  positions[0].value = 4000
+  pool.update()
+
+  test(`${info()}: If the original state change, the "index" set follows`,
+    activatedPositionsAreEquivalentTo([
+      4000,
+    ]))
+
+  positions[1].value = 4001
+  pool.update()
+
+  test(`${info()}: ...follows again`,
+    activatedPositionsAreEquivalentTo([
+      4000, 4001,
+    ]))
+
+  positions[0].value = 0
+  positions[2].value = 4002
+  pool.update()
+
+  test(`${info()}: ...and again`,
+    activatedPositionsAreEquivalentTo([
+      4001, 4002,
+    ]))
+
+  state.head = 3006
+  state.distanceMax = 20
+  pool.update()
+
+  test(`${info()} Rollback to 3006 to prepare the next test.`,
+    activatedPositionsAreEquivalentTo([
+      3004, 3005, 3006, 3007, 3008,
+    ]))
+
+  pool.poolSize = 20
+  pool.update()
+
+  test(`${info()} Pool size has changed (increase)!`,
+    activatedPositionsAreEquivalentTo([
+      3000, 3001, 3002, 3003, 3004, 3005, 3006, 3007, 3008,
+    ]))
+
+  pool.poolSize = 1
+  pool.update()
+
+  test(`${info()} Pool size has changed (decrease)!`,
+    activatedPositionsAreEquivalentTo([
+      3006,
+    ]))
+
+  state.head = 0
+  pool.poolSize = 4
+  positions.forEach((position, index) => position.value = index)
+  pool.update()
+
+  test(`${info()} Complex edge case (head & positions has changed)`,
+    activatedPositionsAreEquivalentTo([
+      0, 1, 2, 3,
+    ]))
+
+  testDone()
 }
