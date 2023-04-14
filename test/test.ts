2780a365a62cf5af398d4720d9100ed417ea2909
jniac <joseph.merdrignac@gmail.com>
2023-04-14 06:19:16 +0200

    getCurrentTestName

--- test/test.ts
+++ test/test.ts
@@ -1,36 +1,48 @@
 
-export const createTest = (name: string) => {
-  const testState = {
-    name,
-    count: 0,
+export const createTest = (sessionName: string) => {
+  const state = {
+    sessionName,
+    currentTestName: '...',
+    stepCount: 0,
     failCount: 0,
   }
 
-  const test = (title: string, ...tests: (readonly [boolean, string])[]) => {
+  const test = (testName: string, ...tests: (readonly [boolean, string])[]) => {
+    state.currentTestName = testName
     const success = tests.every(([success]) => success)
-    console.assert(success, `#${testState.count} ${title}`)
+    if (state.failCount === 0) {
+      console.assert(success, `#${state.stepCount} ${testName}`)
+    }
     if (!success) {
-      testState.failCount++
-      for (const [success, failReason] of tests) {
-        if (!success) {
-          console.error(failReason)
+      if (state.failCount === 0) {
+        for (const [success, failReason] of tests) {
+          if (!success) {
+            console.error(failReason)
+          }
         }
       }
+      state.failCount++
     }
-    testState.count++
+    state.stepCount++
   }
 
-  const testDone = () => {
-    const { name, count, failCount } = testState
+  const getCurrentTestName = () => {
+    return state.currentTestName
+  }
+
+  const testDone = (): boolean => {
+    const { sessionName, stepCount, failCount } = state
     if (failCount === 0) {
-      console.log(`Test %c"${name}"%c succeeded! (${count})`, 'color: yellow', '')
+      console.log(`Test %c"${sessionName}"%c succeeded! (${stepCount})`, 'color: yellow', '')
     } else {
-      console.log(`Test %c"${name}"%c has failed. (${failCount} fails over ${count})`, 'color: yellow', '')
+      console.log(`Test %c"${sessionName}"%c has failed. (${failCount} fails over ${stepCount})`, 'color: yellow', '')
     }
+    return failCount === 0
   }
 
   return {
     test,
+    getCurrentTestName,
     testDone,
   }
 }
