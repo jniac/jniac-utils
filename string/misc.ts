170c033f81e5c0a6cd36a7b979f01fa8918b96ba
jniac <joseph.merdrignac@gmail.com>
2023-06-07 12:29:56 +0200

    string utils: firstWord, firstLine

--- string/misc.ts
+++ string/misc.ts
@@ -2,3 +2,7 @@
 // eslint-disable-next-line no-useless-escape
 const isEmailRe = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
 export const isEmail = (str: string) => isEmailRe.test(str)
+
+export const firstWord = (str: string) => str.match(/\b.*\b/)?.[0] ?? ''
+
+export const firstLine = (str: string) => str.match(/.*\n/)?.[0] ?? ''
