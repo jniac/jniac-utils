6800c7b5703535012043b2b16f2d7ede5454b3ee
jniac <joseph.merdrignac@gmail.com>
2024-06-20 09:25:13 +0200

    forget Chrome Yahaa...

--- scripts/misc/browser-start.js
+++ scripts/misc/browser-start.js
@@ -8,7 +8,7 @@ const { spawn } = require('child_process')
 // @ts-ignore
 const os = require('os')
 
-const [,,url = ''] = process.argv
+const [, , url = ''] = process.argv
 
 /**
  * 
@@ -24,9 +24,9 @@ const trySpawn = (...spawnArgs) => new Promise(resolve => {
 const main = async () => {
   if (os.type() === 'Darwin') {
     // Ok you're right, it's a weird option, it allows me to run a Google Chrome instance dedicated to development.
-    if ((await trySpawn('/Applications/Google Chrome Yahaaa!.app/Contents/MacOS/Google Chrome', [`--user-data-dir=${os.homedir()}/chrome-yahaa`, url])).ok) {
-      return
-    }
+    // if ((await trySpawn('/Applications/Google Chrome Yahaaa!.app/Contents/MacOS/Google Chrome', [`--user-data-dir=${os.homedir()}/chrome-yahaa`, url])).ok) {
+    //   return
+    // }
     if ((await trySpawn('/Applications/Google Chrome Dev.app/Contents/MacOS/Google Chrome Dev', [url])).ok) {
       return
     }
